0bf00f94cba998c09f48a08fe10d9d9735c571e3
goudete <goudetenrique@gmail.com>
2023-05-13 18:07:02 -0400

    build error

--- src/components/SubHeadings/RegisterSubHeading.tsx
+++ src/components/SubHeadings/RegisterSubHeading.tsx
@@ -2,7 +2,7 @@
 const RegisterSubHeading: React.FC = () => {
   return (
     <header className="flex flex-col justify-between items-center p-1">
-      <h1 className="text-center mx-auto text-xl font-semibold">What's your number?</h1>
+      <h1 className="text-center mx-auto text-xl font-semibold">What is your number?</h1>
       <h3 className="text-center mx-auto text-md font-semibold text-neutral-700 max-w-[60%] mt-5">You will not be able to login without a valid phone number.</h3>
     </header>
   );
