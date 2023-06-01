45ae745ce43eee08e034dd8d723cf6f59a438154
goudete <goudetenrique@gmail.com>
2023-05-31 20:24:35 -0400

    title in head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <title>Facely AI 🤌🏽</title>
       <Head>
+        <title>Facely AI 🤌🏽</title>
         <meta property="og:title" content="Facely AI 🤌🏽" />
         <meta property="og:description" content="Create your AI avatars" />
         <meta property="og:image" content="https://public-michelangelo-ai.s3.amazonaws.com/image8.png" />
