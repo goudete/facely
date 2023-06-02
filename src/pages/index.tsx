bc7214a196183fe0f4fdeafca340ba09c20e424d
goudete <goudetenrique@gmail.com>
2023-06-02 17:47:14 -0400

    miel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ const images = [
   { src: 'https://public-michelangelo-ai.s3.amazonaws.com/demo5.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/rau.jpeg', alt: 'Image 5' },
   { src: 'https://public-michelangelo-ai.s3.amazonaws.com/ale-avatar.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/ale-raw.png', alt: 'Image 6' },
   { src: 'https://public-michelangelo-ai.s3.amazonaws.com/demo3.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/wa.png', alt: 'Image 6' },
+  { src: 'https://public-michelangelo-ai.s3.amazonaws.com/image8.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/miel.png', alt: 'Image 6' },
   { src: 'https://public-michelangelo-ai.s3.amazonaws.com/luis_got.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/luis_raw_square_crop.jpg', alt: 'Image 2' },
   { src: 'https://public-michelangelo-ai.s3.amazonaws.com/alfonso_dream_portait.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/alfonso_raw_square_crop.jpeg', alt: 'Image 1' },
 ];
