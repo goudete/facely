34bcd94de5b41ae0a40cfe7d51809c14539a0dec
goudete <goudetenrique@gmail.com>
2023-06-02 17:11:19 -0400

    updating landing page images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,10 @@ import GetStartedButton from '@/components/Buttons/GetStartedButton';
 
 const images = [
   { src: 'https://public-michelangelo-ai.s3.amazonaws.com/demo5.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/rau.jpeg', alt: 'Image 5' },
-  { src: 'https://public-michelangelo-ai.s3.amazonaws.com/alfonso_dream_portait.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/alfonso_raw_square_crop.jpeg', alt: 'Image 1' },
+  { src: 'https://public-michelangelo-ai.s3.amazonaws.com/ale-avatar.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/ale-raw.png', alt: 'Image 6' },
+  { src: 'https://public-michelangelo-ai.s3.amazonaws.com/demo3.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/wa.png', alt: 'Image 6' },
   { src: 'https://public-michelangelo-ai.s3.amazonaws.com/luis_got.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/luis_raw_square_crop.jpg', alt: 'Image 2' },
+  { src: 'https://public-michelangelo-ai.s3.amazonaws.com/alfonso_dream_portait.png', original: 'https://public-michelangelo-ai.s3.amazonaws.com/alfonso_raw_square_crop.jpeg', alt: 'Image 1' },
 ];
 
 const Home: NextPage = () => {
