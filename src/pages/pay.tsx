6ba47252c5cfa5c6431c19a8ca9743a7b0c39e6e
goudete <goudetenrique@gmail.com>
2023-06-02 13:14:03 -0400

    new payment flow

--- src/pages/pay.tsx
+++ src/pages/pay.tsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { useStripe, PaymentRequestButtonElement } from '@stripe/react-stripe-js';
 import { PaymentRequest } from '@stripe/stripe-js';
 import Header from '@/components/Header';
 
 const Pay: NextPage = () => {
+  const router = useRouter();
+  const { number, folder, gender, theme } = router.query;
   const stripe = useStripe();
   const [paymentRequest, setPaymentRequest] = useState<PaymentRequest | null>(null);
+  const [stripeError, setStripeError] = useState<boolean>(false);
 
   useEffect(() => {
     if (stripe) {
@@ -53,14 +57,52 @@ const Pay: NextPage = () => {
 
           if (error) {
             ev.complete('fail');
+            return setStripeError(true);
           } else if (paymentIntent.status === 'requires_action') {
             ev.complete('success');
             stripe.confirmCardPayment(clientSecret);
+
+            router.push({
+              pathname: '/home',
+              query: { number },
+            });
+
+            fetch('/api/generate', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                phoneNumber: number,
+                folderName: folder,
+                theme,
+                gender
+              }),
+            });
           } else {
             ev.complete('success');
+
+            router.push({
+              pathname: '/home',
+              query: { number },
+            });
+
+            fetch('/api/generate', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                phoneNumber: number,
+                folderName: folder,
+                theme,
+                gender
+              }),
+            });
           }
         } else {
           ev.complete('fail');
+          return setStripeError(true);
         }
       });
     }
@@ -83,6 +125,13 @@ const Pay: NextPage = () => {
         <header className="flex justify-between items-center p-1">
           <h1 className="text-center mx-auto text-xl">Create Avatars</h1>
         </header>
+        {stripeError && (
+          <div className="flex justify-between items-center p-1 mt-12">
+            <h1 className="text-center mx-auto max-w-[90%] rounded-md bg-red-700 p-3">
+              Error with payment. Please try again.
+            </h1>
+          </div>
+        )}
         <div className="flex flex-col px-4 justify-between items-center mt-12 w-full">
           <div className="flex flex-col items-start space-y-4 w-10/12 md:max-w-[45%] flex-wrap rounded-lg bg-indigo-900 p-6">
             <div className='flex flex-row justify-center'>
